41c9e4e3c7cee559d3575b2dff0452d5e830bca2
MD-Nazim-Uddin-Khan <mdnazimuddin5500@gmail.com>
2021-05-19 15:35:29 +0600

    first commit5

---
+++ src/compenent/firebase.Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC1KpqjPJqD1tf0x744qfHVd-1wUAvClKQ",
+    authDomain: "volunteer-network-c1e6c.firebaseapp.com",
+    projectId: "volunteer-network-c1e6c",
+    storageBucket: "volunteer-network-c1e6c.appspot.com",
+    messagingSenderId: "981951190414",
+    appId: "1:981951190414:web:d66c01988c73aa5a3b40df"
+  };
+
+  export default firebaseConfig;
